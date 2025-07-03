c89b9a92c8065603a01f24b02ce39060bf0ebffa
Aki-1407 <im.deepak1407@gmail.com>
2025-07-04 00:19:38 +0530

    Navbar transistions PR 1

--- Landing-Page/script.js
+++ Landing-Page/script.js
@@ -1,11 +1,29 @@
 const navbar = document.querySelector(".landing-page-nav");
+const branding = document.querySelector(".branding");
+const signupButtons = document.querySelector(".join-or-login");
+const menu = document.querySelector(".nav-menu")
+const aboutScroll = document.getElementById("about");
 
 // navbar hiding on scroll
 window.addEventListener('scroll', () => {
     if(window.scrollY > 500){
+        menu.classList.add("hidden");
+        signupButtons.classList.add("hidden");
         navbar.classList.add("hidden");
+        branding.classList.add("hidden");
+        
     }
-    else {
+    if(window.scrollY < 10) {
         navbar.classList.remove("hidden");
+        menu.classList.remove("hidden");
+        branding.classList.remove("hidden");
+        signupButtons.classList.remove("hidden");
     }
 });
+
+
+branding.addEventListener("click", () => {
+    if(window.scrollY > 0){
+        
+    }
+})
