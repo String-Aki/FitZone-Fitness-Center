5a58e6670a7e732cb4d90c15cd3124ecb019a116
Aki-1407 <im.deepak1407@gmail.com>
2025-07-05 01:25:27 +0530

    1 Testimonials section added. 2 Buy membership buttons linked to login. 3 Navbar Updated 4 dynamically generating star icons

--- Landing-Page/script.js
+++ Landing-Page/script.js
@@ -1,23 +1,14 @@
 const navbar = document.querySelector(".landing-page-nav");
-// const branding = document.querySelector(".branding");
-// const signupButtons = document.querySelector(".join-or-login");
-// const menu = document.querySelector(".nav-menu")
-
 const cards = document.querySelectorAll(".the-about-card");
+const stars = document.querySelectorAll(".stars");
 
 // navbar hiding on scroll
 window.addEventListener("scroll", () => {
   if (window.scrollY > 600) {
-    // menu.classList.add("hidden");
-    // signupButtons.classList.add("hidden");
-    // branding.classList.add("hidden");
     navbar.classList.add("hidden");
   }
   if (window.scrollY < 10) {
     navbar.classList.remove("hidden");
-    // menu.classList.remove("hidden");
-    // branding.classList.remove("hidden");
-    // signupButtons.classList.remove("hidden");
   }
 });
 
@@ -31,3 +22,12 @@ cards.forEach((card) => {
     card.classList.toggle("clicked");
   });
 });
+
+// Generating Stars
+stars.forEach((starContainer) => {
+  for(let i = 0; i < 5; i++){
+    const starIcon = document.createElement('i');
+    starIcon.classList.add("fas", "fa-star");
+    starContainer.appendChild(starIcon);
+  }
+})
