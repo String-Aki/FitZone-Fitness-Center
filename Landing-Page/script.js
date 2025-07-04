6672920c102e1e4ff51c32d38a90a7ea181ff966
Aki-1407 <im.deepak1407@gmail.com>
2025-07-05 02:30:58 +0530

    Dynamic Navbar with pseudo nav logo

--- Landing-Page/script.js
+++ Landing-Page/script.js
@@ -1,14 +1,17 @@
 const navbar = document.querySelector(".landing-page-nav");
 const cards = document.querySelectorAll(".the-about-card");
 const stars = document.querySelectorAll(".stars");
+const pseudoNav = document.querySelector(".pseudo-nav");
 
 // navbar hiding on scroll
 window.addEventListener("scroll", () => {
   if (window.scrollY > 600) {
     navbar.classList.add("hidden");
+    pseudoNav.classList.add("show");
   }
   if (window.scrollY < 10) {
     navbar.classList.remove("hidden");
+    pseudoNav.classList.remove("show");
   }
 });
 
@@ -25,9 +28,10 @@ cards.forEach((card) => {
 
 // Generating Stars
 stars.forEach((starContainer) => {
-  for(let i = 0; i < 5; i++){
-    const starIcon = document.createElement('i');
+  for (let i = 0; i < 5; i++) {
+    const starIcon = document.createElement("i");
     starIcon.classList.add("fas", "fa-star");
     starContainer.appendChild(starIcon);
   }
-})
+});
+
