4f2c21bac0a30164a697a621c269c38e3aa5ea6d
Aki-1407 <im.deepak1407@gmail.com>
2025-07-05 18:57:47 +0530

    Footer Added

--- Landing-Page/script.js
+++ Landing-Page/script.js
@@ -1,37 +1,40 @@
-const navbar = document.querySelector(".landing-page-nav");
-const cards = document.querySelectorAll(".the-about-card");
-const stars = document.querySelectorAll(".stars");
-const pseudoNav = document.querySelector(".pseudo-nav");
+document.addEventListener("DOMContentLoaded", () => {
 
-// navbar hiding on scroll
-window.addEventListener("scroll", () => {
-  if (window.scrollY > 600) {
-    navbar.classList.add("hidden");
-    pseudoNav.classList.add("show");
-  }
-  if (window.scrollY < 10) {
-    navbar.classList.remove("hidden");
-    pseudoNav.classList.remove("show");
-  }
-});
+  const navbar = document.querySelector(".landing-page-nav");
+  const cards = document.querySelectorAll(".the-about-card");
+  const stars = document.querySelectorAll(".stars");
+  const pseudoNav = document.querySelector(".pseudo-nav");
 
-// Cards Flipping
-cards.forEach((card) => {
-  card.addEventListener("mouseleave", () => {
-    card.classList.remove("clicked");
+  // navbar hiding on scroll
+  window.addEventListener("scroll", () => {
+    if (window.scrollY > 600) {
+      navbar.classList.add("hidden");
+      pseudoNav.classList.add("show");
+    }
+    if (window.scrollY < 10) {
+      navbar.classList.remove("hidden");
+      pseudoNav.classList.remove("show");
+    }
   });
 
-  card.addEventListener("click", () => {
-    card.classList.toggle("clicked");
+  // Cards Flipping
+  cards.forEach((card) => {
+    card.addEventListener("mouseleave", () => {
+      card.classList.remove("clicked");
+    });
+
+    card.addEventListener("click", () => {
+      card.classList.toggle("clicked");
+    });
   });
-});
 
-// Generating Stars
-stars.forEach((starContainer) => {
-  for (let i = 0; i < 5; i++) {
-    const starIcon = document.createElement("i");
-    starIcon.classList.add("fas", "fa-star");
-    starContainer.appendChild(starIcon);
-  }
+  // Generating Stars
+  stars.forEach((starContainer) => {
+    for (let i = 0; i < 5; i++) {
+      const starIcon = document.createElement("i");
+      starIcon.classList.add("fas", "fa-star");
+      starContainer.appendChild(starIcon);
+    }
+  });
+  
 });
-
