ea7b5bb50bfeb851b83775cf5ec0f0ba24f417ae
Aki-1407 <im.deepak1407@gmail.com>
2025-07-16 14:16:20 +0530

    Registration file has been connected with the database

---
+++ Registration-Page/register.php
@@ -0,0 +1,142 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="style.css" />
+    <title>Create Account</title>
+  </head>
+  <body>
+    <img
+      src="../Assets/registration-page-assets/register-page-image.png"
+      alt="main-image"
+      class="main-img"
+    />
+    <section class="sign-up-container">
+
+      <a href="../index.html" class="back-home fas fa-arrow-left"></a>
+
+      <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post" class="registration-form">
+        <h1 class="register-heading">Sign Up</h1>
+        <p class="register-p">
+          Create your account and start your fitness journey with us today.
+        </p>
+
+        <div class="name-fields">
+          <div>
+            <label for="first-name-field">First Name</label>
+            <input type="text"
+             name="firstName"
+             id="first-name-field"
+             required/>
+          </div>
+
+          <div>
+            <label for="last-name-field">Last Name</label>
+            <input type="text" 
+            name="lastName" 
+            id="last-name-field" 
+            required/>
+          </div>
+
+        </div>
+
+        <label for="phone-number-field">Phone Number</label>
+        <input
+          type="tel"
+          name="phoneNumber"
+          id="phone-number-field"
+          placeholder="e.g +(55) 123 4567 890"
+          required
+        />
+
+        <label for="email-field">Email address</label>
+        <input
+          type="email"
+          name="email"
+          id="email-field"
+          placeholder="e.g example@gmail.com"
+          required
+        />
+
+        <div class="password-container">
+          <label for="password-field">Password</label>
+          <i class="fa-solid fa-eye pass-toggle"></i>
+        </div>
+
+        <input
+          type="password"
+          name="password"
+          id="password-field"
+          class="password-field"
+          pattern="^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@#$%])[A-Za-z\d@#$%]{8,}$"
+          minlength="8"
+          title="Password must be at least 8 characters and include an uppercase letter, lowercase letter, number, and special character (@#$%)."
+          required
+        />
+        <p class="password-constraints">
+          Use 8 or more characters with a mix of uppercase, lowercase, numbers & symbols
+        </p>
+
+        <button type="submit" name="signup" class="submit-button">Sign up</button>
+
+        <div class="account-exist">
+          <p>
+            Already have an ccount?
+            <a href="../Sign-In-Page/index.html">Log in</a>
+          </p>
+        </div>
+
+      </form>
+    </section>
+    
+    <?php
+    session_start();
+    session_regenerate_id();
+    include("../includes/dbconnect.php");
+    error_reporting(E_ALL);
+    
+    if(isset($_POST["signup"])){
+      $firstn = trim($_POST["firstName"]);
+      $lastn = trim($_POST["lastName"]);
+      $phonenum = trim($_POST["phoneNumber"]);
+      $email = trim($_POST["email"]);
+      $passw = trim($_POST["password"]);
+      
+      $hashed_password = password_hash($passw , PASSWORD_DEFAULT);
+
+      $stmt = $conn->prepare("INSERT INTO users (First_Name, Last_Name, Phone, Email, Password) VALUES (?, ?, ?, ?, ?)");
+
+      if(!$stmt){
+        die("Prepare failed: " . $conn->error);
+      }
+
+      $stmt->bind_param("ssiss", $firstn, $lastn, $phonenum, $email, $hashed_password);
+
+      if($stmt->execute()){
+        echo '<script type="text/javascript">alert("Account Created Successfully. Now you can login");
+        window.location.href="../Sign-In-Page/index.html";
+        </script>';
+      }
+
+      else{
+        echo "Error executing query: " . $stmt->error;
+      }
+      $stmt->close();
+    }
+
+    $conn->close();
+    ?>
+
+    <script
+      src="https://kit.fontawesome.com/15767cca17.js"
+      crossorigin="anonymous"
+    ></script>
+
+    <script 
+      type="text/javascript" 
+      src="./script.js"
+    ></script>
+  </body>
+</html>
+
--- includes/dbconnect.php
+++ includes/dbconnect.php
@@ -1,16 +1,12 @@
 <?php 
-
 $server="localhost";
 $user="root";
 $password="";
 $dbase="fitzone_7306";
 
 $conn=mysqli_connect($server, $user, $password, $dbase );
-
 if(!$conn){
-
-	die("Connection failed: ".mysqli_error());
-
+	die();
 }
-echo "Connected successfully";
+echo '<script type="text/javascript">console.log("Database Connected successfully");</script>';
  ?>
