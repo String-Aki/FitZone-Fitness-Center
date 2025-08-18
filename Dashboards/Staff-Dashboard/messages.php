9d01b1a384f0db70bdcc9c7ef6ed76d7fbeff8ac
Aki-1407 <im.deepak1407@gmail.com>
2025-08-18 22:06:33 +0530

    Fully functional messages section implemented

---
+++ Dashboards/Staff-Dashboard/messages.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Customer Messages</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <?php include("../components/staff-dashboard-side-bar.php");?>
+    
+    <main>
+
+    <?php include("../components/staff-dashboard-header.php");?>
+
+        <section class="staff-dashboard-section">
+            <div class="table-container">
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Customer ID</th>
+                            <th>Name</th>
+                            <th>Subject</th>
+                            <th>Inquiry</th>
+                            <th>Time of Inquiry</th>
+                            <th>Response</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+
+                    <?php
+                        $fetch_messages = "SELECT messages.*, users.First_Name, users.Last_Name, users.User_ID FROM messages JOIN users ON users.User_ID = messages.User_ID JOIN trainers ON messages.Recipient_ID = trainers.Trainer_ID WHERE trainers.User_ID = '".$_SESSION['user_id']."'";
+
+                        $messages = $conn->query($fetch_messages);
+                        
+                        if($messages->num_rows > 0 ){
+
+                            while($row = $messages->fetch_assoc()){
+
+                                $time_of_inquiry = date('m-d-Y g:i A', strtotime($row['Created_At']));
+
+                                if (substr($row['Upload_Path'], 0, 3) === '../') {
+                                    $path = substr($row['Upload_Path'], 3);
+                                    }
+                                    else {
+                                    $path = "";
+                                    }
+                                
+                                $is_disabled = empty($row['Upload_Path']);
+                                $is_responded = !empty($row['Response']);
+                                echo 
+                                '
+                                <tr>
+                            <td>#'.htmlspecialchars($row['User_ID']).'</td>
+                            <td class="full-name">'.htmlspecialchars($row['First_Name']." ".$row['Last_Name']).'</td>
+                            <td>'.htmlspecialchars($row['Topic']).'</td>
+                            <td>'.htmlspecialchars($row['Message']).'</td>
+                            <td>'.htmlspecialchars($time_of_inquiry).'</td>
+                            <td class="' . ($is_responded ? "" : "not-replied") . '">' . ($is_responded ? htmlspecialchars($row['Response']) : "Not Replied") . '</td>
+                            <td>
+                                <a href="'.htmlspecialchars($path).'" 
+                                class="action-button '.($is_disabled ? 'disabled-link' : '').'" 
+                                '.($is_disabled ? ' onclick="return false;"' : '').' download>
+                                <i class="fas fa-file-arrow-down download-icon"></i>
+                                </a>
+
+                            </td>
+                        </tr>
+                                ';
+                            }
+                        }
+                    ?>                        
+                    </tbody>
+                </table>
+            </div>
+        </section>
+    </main>
+<script
+      src="https://kit.fontawesome.com/15767cca17.js"
+      crossorigin="anonymous"
+    ></script>
+</body>
+</html>
