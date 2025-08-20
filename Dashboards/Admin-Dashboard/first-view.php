1d06b337e82eefa163b655571d53cb85e54c55c2
Aki-1407 <im.deepak1407@gmail.com>
2025-08-20 10:40:46 +0530

    link updated

--- Dashboards/Admin-Dashboard/first-view.php
+++ Dashboards/Admin-Dashboard/first-view.php
@@ -7,13 +7,13 @@
     <link rel="stylesheet" href="./style.css">
 </head>
 <body>
-    <?php include("../components/admin-menu.php")?>
+     <?php include("../components/admin/admin-menu.php")?>
     <section class="dashboard-selection">
         <ul class="selection-links">
             <li class="links"><a href="./create-account.php?selected=Create Account">Create Account</a><i class="fas fa-arrow-right"></i></li>
             <li class="links"><a href="./manage-accounts.php?selected=Manage Accounts">Manage Accounts</a><i class="fas fa-arrow-right"></i></li>
             <li class="links"><a href="./broadcast.php?selected=Broadcast">Broadcast</a><i class="fas fa-arrow-right"></i></li>
-            <li class="links"><a href="">Everything Else</a><i class="fas fa-arrow-right"></i></li>
+            <li class="links"><a href="./Everything-Else/dashboard.php">Everything Else</a><i class="fas fa-arrow-right"></i></li>
         </ul>
     </section>
 </body>
