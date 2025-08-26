03ec35f45b30cc974ddcce15a4c718b1395c28c1
Aki-1407 <im.deepak1407@gmail.com>
2025-08-27 03:11:35 +0530

    error logging file added

--- includes/dbconnect.php
+++ includes/dbconnect.php
@@ -1,4 +1,15 @@
 <?php 
+
+$error_log_path = __DIR__ . "/../logs/errors.log";
+
+ini_set('display_errors', 0);
+ini_set('display_startup_errors', 0);
+
+ini_set('log_errors', 1);
+ini_set('error_log', $error_log_path);
+
+error_reporting(E_ALL);
+
 $server="localhost";
 $user="root";
 $password="";
